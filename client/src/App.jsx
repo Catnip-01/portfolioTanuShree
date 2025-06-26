5ea8fed090fb375ed39008b20edf45da920ba8cf
Catnip <72100668+Catnip-01@users.noreply.github.com>
2025-06-26 14:55:33 +0000

    actual theme button removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -71,7 +71,7 @@ const Header = () => {
                     {/* <ThemeToggle /> */}
                 </div>
                 <div className="md:hidden flex items-center">
-                    <ThemeToggle />
+                    {/* <ThemeToggle /> */}
                     <button onClick={() => setIsOpen(!isOpen)} className="text-pale-gold focus:outline-none ml-4"><svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">{isOpen ? <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /> : <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />}</svg></button>
                 </div>
             </div>
